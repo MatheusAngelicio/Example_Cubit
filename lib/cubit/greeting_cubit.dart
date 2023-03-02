925d6463990d40ef642f9dab166d5b52d98ad1bd
Premiersoft <premiersoft@Premiersofts-MacBook-Pro-5.local>
2023-03-02 12:40:23 -0300

    Finalizar integracao com cubit

--- lib/cubit/greeting_cubit.dart
+++ lib/cubit/greeting_cubit.dart
@@ -2,5 +2,24 @@ import 'package:bloc/bloc.dart';
 import 'package:example_cubit/cubit/greeting_state.dart';
 
 class GreetingCubit extends Cubit<GreetingState> {
-  GreetingCubit(): super(GreetingInitial());
+  int howManyTimes = 0;
+
+  GreetingCubit() : super(GreetingInitial());
+
+  Future<void> getNextGreetingstate() async {
+    howManyTimes++;
+
+    if (howManyTimes == 1) {
+      emit(GreetingSecondTime());
+    } else if (howManyTimes == 2) {
+      emit(GreetingThirdTime());
+    } else {
+      emit(GreetingThereafter());
+    }
+  }
+
+  Future<void> resetCouting() async {
+    howManyTimes = 0;
+    emit(GreetingInitial());
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:example_cubit/cubit/greeting_cubit.dart';
+import 'package:example_cubit/cubit/greeting_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +18,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: BlocProvider<GreetingCubit>(
+        create: (context) => GreetingCubit(),
+        child: HomePage(),
+      ),
     );
   }
 }
@@ -26,15 +32,55 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: Container(
-        alignment: Alignment.center,
-        child: Text(
-          "Olá!",
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              TextMessage(),
+              TextButton(
+                onPressed: () {
+                  final greetingCubit = context.read<GreetingCubit>();
+                  greetingCubit.getNextGreetingstate();
+                },
+                child: Text('Proxima visita'),
+              ),
+              TextButton(
+                onPressed: () {
+                  final greetingCubit = context.read<GreetingCubit>();
+                  greetingCubit.resetCouting();
+                },
+                child: Text('Zerar contagem'),
+              ),
+            ],
+          )),
+    );
+  }
+}
+
+class TextMessage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<GreetingCubit, GreetingState>(
+      builder: (context, state) {
+        String message = '';
+
+        if (state is GreetingInitial) {
+          message = 'Bem vindo';
+        } else if (state is GreetingSecondTime) {
+          message = 'É seu segundo acesso, parabens';
+        } else if (state is GreetingThirdTime) {
+          message = 'Seu terceiro acesso';
+        } else {
+          message = 'Bem vindo novamente';
+        }
+
+        return Text(
+          message,
           style: TextStyle(
             fontWeight: FontWeight.bold,
             fontSize: 25,
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
